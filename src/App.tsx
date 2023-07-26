b07b6680353d532cf3a511f9f95f2cedcff689b7
mrg <rgmengedoth@gmail.com>
2023-07-26 13:56:54 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,19 @@ import './App.css'
     }
 
     function Hello(props: FriendProps) {
-        return<div>
-        <h1>
-            Hello {props.name} i heared you are {props.age} years old!
-        </h1>
-            {(() => {
-                if (props.zuAlt === true){
-                    return <h2>du bist zu alt</h2>
-                } else if (props.zuAlt === false){
-                    return <h2>dein alter ist okay</h2>
-                } else {
-                    return <h2>dein alter wurde nicht angegeben</h2>
-                }
-            })()}
+        return <div>
+            <h1>
+                Hello {props.name} i heared you are {props.age} years old!
+            </h1>
+                {(() => {
+                    if (props.zuAlt === true){
+                        return <h2>du bist zu alt</h2>
+                    } else if (props.zuAlt === false){
+                        return <h2>dein alter ist okay</h2>
+                    } else {
+                        return <h2>dein alter wurde nicht angegeben</h2>
+                    }
+                })()}
     </div>
     }
 
